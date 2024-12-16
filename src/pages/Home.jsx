27e435994cbf9068d2f7ea1d6032ad04e19ed91d
mohamedMorsy <mmrsy1902@gmail.com>
2024-12-17 01:30:10 +0200

    I added the Affordable Pricing component and created a reusable component named SectionTitle.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import AffordablePricing from "../components/AffordablePricing";
 
 export default function Home() {
-	return (
-		<>
-			
-		</>
-	);
+  return (
+    <>
+      <AffordablePricing />
+    </>
+  );
 }
