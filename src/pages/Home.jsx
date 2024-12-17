a8e633fcf5099e313217577efd6c76315d8995b2
mohamedMorsy <mmrsy1902@gmail.com>
2024-12-17 02:32:12 +0200

    I added the WorkingExpert and Feedback components, and also added the FeedbackCard as a reusable component within the Feedback component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import AffordablePricing from "../components/AffordablePricing";
+import WorkingExperts from "../components/WorkingExperts";
+import Specialist from "../components/Specialist";
+import FeedBack from "../components/FeedBack";
 
 export default function Home() {
   return (
     <>
       <AffordablePricing />
+	  <WorkingExperts/>
+	  <Specialist/>
+	  <FeedBack/>
     </>
   );
 }
