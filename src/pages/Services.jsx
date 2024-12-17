f61a72d9ec9c96f14b3f202faca74b1324e89934
ibrahim <53229091+IbrahimOraby@users.noreply.github.com>
2024-12-17 18:17:16 +0200

    feat/applying banner to all pages except blog and blog-details (#39)

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,34 +1,37 @@
-import { Link } from "react-router"
-import "../assets/styles/services.css"
-import Card from "../components/services/Card"
+import { Link } from "react-router";
+import "../assets/styles/services.css";
+import Card from "../components/services/Card";
+import HeadBanner from "../components/HeadBanner/HeadBanner";
 
 export default function Services() {
 	return (
-		<div className="main-container">
-			<section className="container py-5">
-				<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-gap-4">
-					{[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => (
-						<Card
-							key={item}
-						/>
-					))}
-				</div>
-			</section>
-			<section className="second-section">
-				<div className="container pt-5">
-					<h2 className="text-capitalize fs-1">
-						<b>together</b> we&apos;ll <br />
-						explore <b>new things</b>
-					</h2>
-					<Link
-						href="#"
-						className="primary-btn text-capitalize text-decoration-none fs-6 fw-bold py-3 px-4 rounded-1"
-					>
-						book
-						an appointment {">"}
-					</Link>
-				</div>
-			</section>
-		</div>
-	)
+		<>
+			{/* <!-- banner --> */}
+			<HeadBanner currPage={"Our Services"} />
+
+			<div className="main-container">
+				<section className="container py-5">
+					<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-gap-4">
+						{[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
+							<Card key={item} />
+						))}
+					</div>
+				</section>
+				<section className="second-section">
+					<div className="container pt-5">
+						<h2 className="text-capitalize fs-1">
+							<b>together</b> we&apos;ll <br />
+							explore <b>new things</b>
+						</h2>
+						<Link
+							href="#"
+							className="primary-btn text-capitalize text-decoration-none fs-6 fw-bold py-3 px-4 rounded-1"
+						>
+							book an appointment {">"}
+						</Link>
+					</div>
+				</section>
+			</div>
+		</>
+	);
 }
