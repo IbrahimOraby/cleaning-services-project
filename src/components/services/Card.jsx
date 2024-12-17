63fa643156a85a5c01a75d824ae8cb288250619d
IbrahimOraby <orabyibrahim19@gmail.com>
2024-12-17 20:08:40 +0200

    fix/applied routing to services-details

--- src/components/services/Card.jsx
+++ src/components/services/Card.jsx
@@ -1,17 +1,20 @@
 // import "../../assets/styles/services.css"
+import { Link } from "react-router";
 
 function Card() {
-    return (
-        <div className="col">
-            <div className="card">
-                <div className="icon-container position-absolute">
-                    <img src="assets/images/clean-icon.png" alt="clean icon" />
-                </div>
-                <img src="assets/images/clean.jpg" alt="" className="card-img" />
-                <p className="text-capitalize">office cleaning</p>
-            </div>
-        </div>
-    )
+	return (
+		<div className="col">
+			<div className="card">
+				<div className="icon-container position-absolute">
+					<img src="assets/images/clean-icon.png" alt="clean icon" />
+				</div>
+				<Link to="service-details" className="text-decoration-none text-center">
+					<img src="assets/images/clean.jpg" alt="" className="card-img" />
+					<p className="text-capitalize">office cleaning</p>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
-export default Card
+export default Card;
