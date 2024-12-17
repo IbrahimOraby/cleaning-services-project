1b12982775729d693af1d2863efef3054f7f7834
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-12-17 15:13:13 +0200

    competeing services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,97 +1,17 @@
+import { Link } from "react-router"
+import "../assets/styles/services.css"
+import Card from "../components/services/Card"
+
 export default function Services() {
 	return (
 		<div className="main-container">
 			<section className="container py-5">
 				<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-gap-4">
-					<div className="col">
-						<div className="card">
-							<div className="icon-container position-absolute">
-								<img src="assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">office cleaning</p>
-						</div>
-					</div>
-
-					<div className="col">
-						<div className="card">
-							<div className="icon-container position-absolute">
-								<img src="assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">window cleaning</p>
-						</div>
-					</div>
-
-					<div className="col">
-						<div className="card">
-							<div className="position-absolute icon-container">
-								<img src="assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">floor cleaning</p>
-						</div>
-					</div>
-
-					<div className="col">
-						<div className="card">
-							<div className="position-absolute icon-container">
-								<img src="assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">kitchen cleaning</p>
-						</div>
-					</div>
-
-					<div className="col">
-						<div className="card">
-							<div className="position-absolute icon-container">
-								<img src="assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">car wash</p>
-						</div>
-					</div>
-
-					<div className="col">
-						<div className="card">
-							<div className="position-absolute icon-container">
-								<img src="assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">kitchen cleaning</p>
-						</div>
-					</div>
-
-					<div className="col">
-						<div className="card">
-							<div className="position-absolute icon-container">
-								<img src="assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">kitchen cleaning</p>
-						</div>
-					</div>
-
-					<div className="col">
-						<div className="card">
-							<div className="position-absolute icon-container">
-								<img src="../assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">kitchen cleaning</p>
-						</div>
-					</div>
-
-					<div className="col">
-						<div className="card">
-							<div className="position-absolute icon-container">
-								<img src="../assets/images/clean-icon.png" alt="clean icon" />
-							</div>
-							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
-							<p className="text-capitalize">kitchen cleaning</p>
-						</div>
-					</div>
+					{[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => (
+						<Card
+							key={item}
+						/>
+					))}
 				</div>
 			</section>
 			<section className="second-section">
@@ -100,13 +20,13 @@ export default function Services() {
 						<b>together</b> we&apos;ll <br />
 						explore <b>new things</b>
 					</h2>
-					<a
+					<Link
 						href="#"
 						className="primary-btn text-capitalize text-decoration-none fs-6 fw-bold py-3 px-4 rounded-1"
 					>
 						book
 						an appointment {">"}
-					</a>
+					</Link>
 				</div>
 			</section>
 		</div>
