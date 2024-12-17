3ffb20d3096729147acb8e1368902d7a97cda166
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2024-12-17 14:24:05 +0200

    add

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,114 @@
-import React from "react";
-
 export default function Services() {
-	return <div>S</div>;
+	return (
+		<div className="main-container">
+			<section className="container py-5">
+				<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-gap-4">
+					<div className="col">
+						<div className="card">
+							<div className="icon-container position-absolute">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">office cleaning</p>
+						</div>
+					</div>
+
+					<div className="col">
+						<div className="card">
+							<div className="icon-container position-absolute">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">window cleaning</p>
+						</div>
+					</div>
+
+					<div className="col">
+						<div className="card">
+							<div className="position-absolute icon-container">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">floor cleaning</p>
+						</div>
+					</div>
+
+					<div className="col">
+						<div className="card">
+							<div className="position-absolute icon-container">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">kitchen cleaning</p>
+						</div>
+					</div>
+
+					<div className="col">
+						<div className="card">
+							<div className="position-absolute icon-container">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">car wash</p>
+						</div>
+					</div>
+
+					<div className="col">
+						<div className="card">
+							<div className="position-absolute icon-container">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">kitchen cleaning</p>
+						</div>
+					</div>
+
+					<div className="col">
+						<div className="card">
+							<div className="position-absolute icon-container">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">kitchen cleaning</p>
+						</div>
+					</div>
+
+					<div className="col">
+						<div className="card">
+							<div className="position-absolute icon-container">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">kitchen cleaning</p>
+						</div>
+					</div>
+
+					<div className="col">
+						<div className="card">
+							<div className="position-absolute icon-container">
+								<img src="../assets/images/clean-icon.png" alt="clean icon" />
+							</div>
+							<img src="../assets/images/clean.jpg" alt="" className="card-img" />
+							<p className="text-capitalize">kitchen cleaning</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="second-section">
+				<div className="container pt-5">
+					<h2 className="text-capitalize fs-1">
+						<b>together</b> we&apos;ll <br />
+						explore <b>new things</b>
+					</h2>
+					<a
+						href="#"
+						className="primary-btn text-capitalize text-decoration-none fs-6 fw-bold py-3 px-4 rounded-1"
+					>
+						book
+						an appointment {">"}
+					</a>
+				</div>
+			</section>
+		</div>
+	)
 }
